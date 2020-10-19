80e51fcb4c8d5312fe564ec492f2823b9a688c66
andy-aa <46691193+andy-aa@users.noreply.github.com>
2020-10-19 21:23:09 +0300

    Update ScriptTest.php

--- tests/ScriptTest.php
+++ tests/ScriptTest.php
@@ -16,7 +16,7 @@ class ScriptTest extends TestCase
 
     public function setUp(): void
     {
-        $path = PHP_OS_FAMILY === 'Windows' ? 'Rscript.exe' : 'r';
+        $path = PHP_OS_FAMILY === 'Windows' ? 'Rscript.exe' : 'R';
 
         $this->script = new Script(
             new Runner($path)
